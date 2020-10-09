a3ee3f6acc8a50f16d1d09d1ed8c97ad81e12488
slava <mcrashsm1le@gmail.com>
2020-10-09 17:03:03 +0300

    initial

---
+++ generateContacts.js
@@ -0,0 +1,45 @@
+const fs = require('fs'),
+  fake = require('faker')
+
+const stream = fs.createWriteStream('5k.csv', { flags: 'a' })
+
+const locales = ['EN', 'NL', 'RU']
+
+let tags = ''
+
+const numbers = '0123456789'
+const length = 17
+
+const generate = () => {
+  let result = ''
+  for (let i = 0; i < length; i++) {
+    result = `${result}${numbers.charAt(Math.floor(Math.random() * numbers.length))}`
+  }
+
+  return result
+}
+
+for (let i = 0; i < 20; i++) {
+  tags = tags.concat(`,tag ${fake.lorem.word()}`)
+}
+
+tags = `${tags}\n`
+
+stream.write('full name,phone number,first name,last name,language' + tags)
+
+for (let i = 0; i <= 5000; i++) {
+  stream.write(`${fake.name.findName()},${generate()},${fake.name.firstName()},${fake.name.lastName()},${locales[Math.floor(Math.random() * Math.floor(3))]},${fake.lorem.word()}\n`)
+}
+
+stream.end()
+
+console.log('done')
+
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ */
+function a(a, b) {
+  return a + b
+}
